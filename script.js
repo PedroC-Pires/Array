1d647d1a1ff1ca62a753fe45d448948be3e35bfd
Pedro <127866553+PedroC-Pires@users.noreply.github.com>
2023-08-25 09:27:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let nomes = [];
+let profissoes = [];
+let nome = document.getElementById("nome");
+let prof = document.getElementById("profissao");
+
+function adc(){
+    nomes.push(nome.value.toLowerCase());
+    profissoes.push(prof.value.toLowerCase());
+    console.log(nomes, profissoes);
+    alert("Registro adicionado com sucesso!")
+}
+
+function rmv(){
+    let pos = nomes.indexOf(nome.value.toLowerCase());
+    if(pos !== -1){
+        nomes.splice(pos,1);
+        profissoes.splice(pos,1);
+        alert("Registro removido com sucesso!")
+    }else{
+        console.log("O nome "+nome.value+" não foi encontrado")
+        alert("O nome "+nome.value+" não foi encontrado");
+    }
+    console.log(nomes, profissoes);
+}
